f65b8c78b7089561b0215201c4c776743a48f416
Patrick Beaume <p.beaume@gmail.com>
2023-12-07 11:09:14 +0100

    remove background color

--- app/src/GameDisplay.tsx
+++ app/src/GameDisplay.tsx
@@ -1,6 +1,5 @@
 /** @jsxImportSource @emotion/react */
 import { pointerWithin } from '@dnd-kit/core'
-import { css } from '@emotion/react'
 import { GameTable } from '@gamepark/react-game'
 import { PlayerPanels } from './panels/PlayerPanels'
 
@@ -12,7 +11,7 @@ export const GameDisplay = () => {
       xMax={60} 
       yMin={-24}
       yMax={24} margin={{ top: 7, left: 0, right: 0, bottom: 0 }}
-      css={css`background-color: rgba(255, 255, 255, 0.47)`}
+      //css={css`background-color: rgba(255, 255, 255, 0.47)`}
       />
     <PlayerPanels/>
   </>
