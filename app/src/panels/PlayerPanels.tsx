3443781734d838c54975a9325a395849b636fb43
Patrick Beaume <p.beaume@gmail.com>
2023-12-20 16:52:28 +0100

    reduce player panel

--- app/src/panels/PlayerPanels.tsx
+++ app/src/panels/PlayerPanels.tsx
@@ -1,38 +1,19 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
 import AwimbaweRules from '@gamepark/awimbawe/AwimbaweRules'
-import { getCrowns } from '@gamepark/awimbawe/material/Animal'
-import Heir from '@gamepark/awimbawe/material/Heir'
-import { LocationType } from '@gamepark/awimbawe/material/LocationType'
-import { MaterialType } from '@gamepark/awimbawe/material/MaterialType'
 import { Memory } from '@gamepark/awimbawe/rules/Memory'
-import { RuleId } from '@gamepark/awimbawe/rules/RuleId'
 import { PlayerPanel, SpeechBubble, SpeechBubbleDirection, usePlayerId, usePlayers, useRules } from '@gamepark/react-game'
-import sumBy from 'lodash/sumBy'
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 
 export const PlayerPanels: FC<any> = () => {
   const playerId = usePlayerId() ?? 1
   const players = usePlayers({ sortFromMe: true })
-  const rules = useRules<AwimbaweRules>()!
-
-  const getPlayerCrowns = (player: Heir) => {
-    if (rules.game.rule?.id === RuleId.PrepareNewRound) return 0
-    const cards = rules
-      .material(MaterialType.AnimalCard)
-      .location((location) => location.type === LocationType.PlayerTrickStack || location.type === LocationType.PlayerHyena)
-      .player(player)
-      .getItems()
-
-    return sumBy(cards, (card) => getCrowns(card.id))
-  }
 
   return (
     <>
       {players.map((player) => (
         <PlayerPanel key={player.id} playerId={player.id} css={[panelPosition, player.id === playerId? bottomPosition: topPosition ]}>
-          <div css={crowns}>{getPlayerCrowns(player.id)}</div>
           <StartPlayerChoice player={player.id} />
         </PlayerPanel>
       ))}
@@ -45,24 +26,21 @@ const StartPlayerChoice = ({ player }: { player: number }) => {
   const startPlayer = useRules<AwimbaweRules>()?.remind(Memory.StartPlayer)
   if (!startPlayer || startPlayer !== player) return null
   return (
-    <SpeechBubble direction={SpeechBubbleDirection.TOP_LEFT} css={css`bottom: 69%`}>
+    <SpeechBubble direction={SpeechBubbleDirection.TOP_LEFT}>
       {t('rules.start.choose.me')}
     </SpeechBubble>
   )
 }
 
-const crowns = css`
-  font-size: 3em;
-`
-
 const panelPosition = css`
   position: absolute;
   right: 1em;
   width: 28em;
-  height: 14em;
+  height: 6.7em;
   display: flex;
   align-items: flex-end;
   justify-content: center;
+  border-radius: 3em 1.5em 1.5em 3.2em;
 `
 
 const topPosition = css`
