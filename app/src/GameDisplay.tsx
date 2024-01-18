a901a9b7d85c7510b8182490b87fce1eb074cb62
Patrick Beaume <p.beaume@gmail.com>
2024-01-18 16:53:54 +0100

    add navigation buttons

--- app/src/GameDisplay.tsx
+++ app/src/GameDisplay.tsx
@@ -1,9 +1,9 @@
 /** @jsxImportSource @emotion/react */
 import { pointerWithin } from '@dnd-kit/core'
-import { GameTable } from '@gamepark/react-game'
+import { GameTable, GameTableNavigation } from '@gamepark/react-game'
 import { PlayerPanels } from './panels/PlayerPanels'
 import { NewRoundDialog } from './NewRoundDialog'
-//import {css} from "@emotion/react";
+import {css} from "@emotion/react";
 
 export const GameDisplay = () => {
   return <>
@@ -14,8 +14,22 @@ export const GameDisplay = () => {
       yMin={-24}
       yMax={24} margin={{ top: 7, left: 0, right: 0, bottom: 0 }}
       //css={css`background-color: rgba(255, 255, 255, 0.47)`}
-      />
+    >
+      <GameTableNavigation css={navigationPosition} />
+    </GameTable>
     <PlayerPanels/>
     <NewRoundDialog />
   </>
 }
+
+const navigationPosition = css`
+  position: absolute;
+  left: auto;
+  right: 2.5em;
+  top: 41.5em;
+  height: 14em;
+  flex-direction: column;
+  > button {
+    filter: drop-shadow(0.1em 0.1em 0.05em black);
+  }
+`
