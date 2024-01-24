bf383723dfea2fa4f19c4552881e6d624f4eda05
Patrick Beaume <p.beaume@gmail.com>
2024-01-24 14:27:25 +0100

    add game over help to display detail of winning

--- app/src/header/GameOverRule.tsx
+++ app/src/header/GameOverRule.tsx
@@ -3,7 +3,7 @@ import { css } from '@emotion/react'
 import AwimbaweRules from '@gamepark/awimbawe/AwimbaweRules'
 import Heir from '@gamepark/awimbawe/material/Heir'
 import { Memory, RoundSummary } from '@gamepark/awimbawe/rules/Memory'
-import { usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
+import { Avatar, usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
 import partition from 'lodash/partition'
 import { useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
@@ -25,11 +25,36 @@ export const GameOverRule = () => {
   return (
     <div css={rulesCss}>
       <h2>{t('rules.end')}</h2>
-      <p>{t(iWin? 'rules.end.win.me': 'rules.end.win', { player: winnerName } )}</p>
+      <div css={winnerContainer}>
+        <div css={avatarContainer}><Avatar playerId={winner} css={avatarStyle} /></div>
+        <div css={winnerText}>{t(iWin? 'rules.end.win.me': 'rules.end.win', { player: winnerName } )}</div>
+      </div>
       <RoundSummaryHelp css={smaller} />
     </div>
   )
 }
+
+const avatarContainer = css`
+  width: 2em;
+  height: 2em;
+  padding-right: 2.5em;
+`
+
+const avatarStyle = css`
+  //flex: 1;
+  position: absolute;
+  width: 2em;
+  height: 2em;
+`
+
+const winnerText = css`
+  flex: 6;
+`
+
+const winnerContainer = css`
+  display: flex;
+  align-items: center;
+`
 
 const smaller = css`
   font-size: 0.8em;
