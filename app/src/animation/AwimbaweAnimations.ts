89ec1b849e7ed2ad7d4d96770a94d7b6c952d93f
Patrick Beaume <p.beaume@gmail.com>
2024-01-24 10:24:30 +0100

    reduce animation duration

--- app/src/animation/AwimbaweAnimations.ts
+++ app/src/animation/AwimbaweAnimations.ts
@@ -14,5 +14,21 @@ awimbaweAnimations.when()
   .duration(0.2)
 
 awimbaweAnimations.when()
+  .move((move, context) => isMoveItemType(MaterialType.AnimalCard)(move)
+    && (context.rules.game.items[move.itemType]![move.itemIndex].location.type === LocationType.PlayerTrickStack
+      || move.location.type === LocationType.PlayerTrickStack
+      || move.location.type === LocationType.PlayerHyena)
+  )
+  .duration(0.8)
+
+awimbaweAnimations.when()
+  .move((move, context) => isMoveItemType(MaterialType.AnimalCard)(move) && context.rules.game.items[move.itemType]![move.itemIndex].location.type === LocationType.PlayerColumns && context.rules.game.items[move.itemType]![move.itemIndex].location?.rotation?.y === 1 && !move.location.rotation.y)
+  .duration(0.8)
+
+awimbaweAnimations.when()
+  .move((move, context) => isMoveItemType(MaterialType.AnimalCard)(move) && ((context.rules.game.items[move.itemType]![move.itemIndex].location?.rotation?.z === 1 && !move.location?.rotation?.z) || move.location?.rotation?.z === 1))
+  .duration(0.5)
+
+awimbaweAnimations.when()
     .move(isShuffleItemType(MaterialType.AnimalCard))
     .none()
